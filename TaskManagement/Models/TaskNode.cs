ffd78c431eff46403d1743649fe823db8ae1764c
K33pQu13t <lucky19manrus@gmail.com>
2021-07-19 14:20:20 +0000

    Добавлена главная модель
    Добавлена абстракция элемента. Добавлены валидаторы на get для запланированного и фактического времени задачи

--- TaskManagement/Models/TaskNode.cs
+++ TaskManagement/Models/TaskNode.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TaskManagement.Services;
 
 namespace TaskManagement.Models
 {
-    public class TaskNode
+    public class TaskNode : Node
     {
-        /// <summary>
-        /// наименование задачи
-        /// </summary>
-        public string Title { get; set; }
+        public TaskNode(string _title, string _description, 
+            List<string> _executorList, 
+            int _executionTimePlanned,
+            List<Node> _childrenList = null,
+            Node _parent = null) 
+                :base(_title, _childrenList, _parent)
+        {
+            Description = _description;
+            ExecutorList = _executorList;
+            ExecutionTimePlanned = _executionTimePlanned;
+
+            TaskState = State.Assigned;
+            RegisterDate = DateTime.Now;
+        }
+
         /// <summary>
         /// описание задачи
         /// </summary>
@@ -31,19 +43,67 @@ namespace TaskManagement.Models
         /// </summary>
         public DateTime RegisterDate { get; set; }
         /// <summary>
-        /// плановая трудоёмкость задачи
+        /// плановая трудоёмкость задачи в часах
         /// </summary>
-        public int ExecutionTimePlanned { get; set; }
+        public int ExecutionTimePlanned 
+        {
+            get 
+            {
+                //сумма запланированного времени всех элементов
+                int sum = executionTimePlanned;
+                foreach (TaskNode node in ChildrenList)
+                {
+                    sum += node.ExecutionTimePlanned;
+                }
+                return sum;
+            }
+            private set
+            {
+                executionTimePlanned = value;
+            }
+        }
+        private int executionTimePlanned;
         /// <summary>
-        /// фактическое время выполнения
+        /// фактическое время выполнения в часах
         /// </summary>
-        public int ExecutionTimeActual { get; set; }
+        public int ExecutionTimeActual 
+        {
+            get
+            {
+                //сумма фактического времени всех элементов
+                int sum = executionTimeActual;
+                foreach (TaskNode node in ChildrenList)
+                {
+                    sum += node.ExecutionTimeActual;
+                }
+                return sum;
+            }
+            private set
+            {
+                executionTimeActual = value;
+            }
+        }
+        private int executionTimeActual;
         /// <summary>
         /// дата завершения задачи
         /// </summary>
         public DateTime CompleteDate { get; set; }
 
+        public override bool Remove(Node node)
+        {
+            if (node == this && node.IsTerminal())
+            {
+                //todo: добавить логику удаления из бд
+                return true;
+            }
+            return false;
+        }
 
+        public override void Add(Node node)
+        {
+            node.Parent = this;
+            ChildrenList.Add(node);
+        }
 
         public enum State
         {
---
+++ TaskManagement/Services/Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Services
+{
+    abstract public class Node
+    {
+        /// <summary>
+        /// наименование компонента
+        /// </summary>
+        public string Title { get; set; }
+
+        public Node Parent { get; set; }
+        public List<Node> ChildrenList { get; set; }
+        public Node(string _title, List<Node> _childrenList, Node _parent)
+        {
+            Title = _title;
+            ChildrenList = _childrenList != null 
+                ? _childrenList 
+                : new List<Node>();
+
+            Parent = _parent;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>true если узел не является наследником</returns>
+        public bool IsTerminal()
+        {
+            return Parent == null;
+        }
+
+        public abstract bool Remove(Node node);
+        public abstract void Add(Node node);
+
+    }
+}
